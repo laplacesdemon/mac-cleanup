107ce148d4c271c56ed82b3ebb4311f539dcef5c
Suleyman Melikoglu <suleymanmelikoglu@gmail.com>
2014-03-01 14:43:00 +0200

    echo statements are added

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+echo "Starting to clean up"
 cd ~/Library/Application\ Support/iPhone\ Simulator/
 for i in $(ls)
 do
   rm -fr $i/Applications/*
 done
 rm -fr ~/Library/Developer/Xcode/DerivedDate/*
+echo "Cleanup is completed"
