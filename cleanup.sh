a21f30a8b005ea6cea2b9d8dd93141eea65e85cb
Suleyman Melikoglu <suleymanmelikoglu@gmail.com>
2014-09-22 18:19:54 +0300

    brew cleanup and mobile backups

--- cleanup.sh
+++ cleanup.sh
@@ -10,4 +10,12 @@ rm -fr ~/Library/Developer/Xcode/DerivedData/*
 
 echo "Cleaning torrentv uploads"
 rm -fr /tmp/torrent-stream
+
+echo "Cleaning mobile back-ups"
+rm -fr /Users/solomon/Library/Application\ Support/MobileSync/Backup
+
+echo "Cleaning brew"
+brew cleanup
+
 echo "Cleanup is completed"
+
